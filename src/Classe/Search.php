7dd16bbdb04598ae991b863bd615cfb5a48b6187
vigreux-joel <vigreux.joel@hotmail.com>
2022-02-04 09:46:43 +0100

    first commit

---
+++ src/Classe/Search.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Classe;
+
+class Search
+{
+
+    /**
+     * @var string
+     */
+    private string $string = '';
+    /**
+     * @var array
+     */
+    private array $category = [];
+
+
+
+
+
+    /**
+     * @return string
+     */
+    public function getString(): string
+    {
+        return $this->string;
+    }
+
+    /**
+     * @param string $string
+     */
+    public function setString(string $string): void
+    {
+        $this->string = $string;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCategory(): array
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param array $category
+     */
+    public function setCategory(array $category): void
+    {
+        $this->category = $category;
+    }
+
+
+}
---
+++ src/Form/SearchType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Mon prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Mon nom'
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Mon adresse email'
+            ])
+            ->add('old_password', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'mapped' => false,
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Votre mot de passe actuel'
+                ]
+            ])
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'constraints' => [
+                    new Length(null, 8),
+                ],
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique',
+                'required' => false,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe (Facultatif)',
+                    'attr' => [
+                        'placeholder' => 'Votre nouveau mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmez votre nouveau mot de passe'
+                    ]
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Mettre à jour"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
